2a6d7565965d6c14d108329426eb928c7690ff40
Solankikrupal <krplsolanki@gmail.com>
2023-05-16 19:46:17 +0530

    Create Model , schema and create some data

--- Basic/controllers/usersController.js
+++ Basic/controllers/usersController.js
@@ -1,12 +1,5 @@
 const bcrypt = require("bcrypt");
-const fsPromises = require("fs").promises;
-const path = require("path");
-const data = {
-  users: require("../data/users.json"),
-  setUser: function (data) {
-    this.users = data;
-  },
-};
+const User = require("../model/User");
 
 const handleNewUser = async (req, res) => {
   const { user, pwd } = req.body;
@@ -16,7 +9,7 @@ const handleNewUser = async (req, res) => {
       .status(400)
       .json({ message: "Username and Password is required" });
 
-  const duplicate = data.users.find((person) => person.user === user);
+  const duplicate = await User.findOne({ username: user }).exec();
   if (duplicate)
     return res.status(409).json({ message: "User already available" });
   try {
@@ -24,15 +17,8 @@ const handleNewUser = async (req, res) => {
     const hspwd = await bcrypt.hash(pwd, 10);
 
     //store new user
-    const newUser = { username: user, password: hspwd };
-
-    //The spread syntax [...array, newArray]
-    data.setUser([...data.users, newUser]);
-
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "data", "users.json"),
-      JSON.stringify(data.users)
-    );
+    const results = await User.create({ username: user, password: hspwd });
+    console.log(results);
     return res.status(200).json({ message: `New User ${user} added` });
   } catch (error) {
     console.log(error);
---
+++ Basic/model/Employee.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+mondule.exports = mongoose.model("Employee", employeeSchema);
